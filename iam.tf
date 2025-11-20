b936338f59387a9bd1f5850c44589dd0753e7243
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2025-11-20 12:58:46 +0800

    [PFMENG-3970] chore: Update TF AWS and Helm providers besides chart version

--- iam.tf
+++ iam.tf
@@ -1,9 +1,9 @@
 module "lb_controller_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.59.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+  version = "~> 6.0"
 
-  role_name_prefix = coalesce(var.iam_role_name, "${var.cluster_name}-lb-")
-  role_description = "EKS Cluster ${var.cluster_name} LoadBalancer Controller"
+  name        = coalesce(var.iam_role_name, "${var.cluster_name}-lb")
+  description = "EKS Cluster ${var.cluster_name} LoadBalancer Controller"
 
   attach_load_balancer_controller_policy = true
   oidc_providers = {
